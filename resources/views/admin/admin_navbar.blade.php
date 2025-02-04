045e01603700026cdb2e09624c985007803f294a
kclementir <124416160+kclementir@users.noreply.github.com>
2025-02-04 10:15:59 +0800

    admin navbar

---
+++ resources/views/admin/admin_navbar.blade.php
@@ -0,0 +1,54 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Dashboard</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('admindashboard') ? 'active text-white' : '' }}"
+                        href="{{ route('admindashboard') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('adminmanage') ? 'active text-white' : '' }}"
+                        href="{{ route('adminmanage') }}">Manage</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('messages') ? 'active text-white' : '' }}"
+                        href="#">Messages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('notifications') ? 'active text-white' : '' }}"
+                        href="#">Notifications</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
+
+<!-- Logout Confirmation Modal -->
+<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <p>Are you sure you want to log out?</p>
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Yes, Logout</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>

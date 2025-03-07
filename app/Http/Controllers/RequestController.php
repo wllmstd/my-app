489d94707a72434c862f08e178a9dd566ba5fa51
John Willemstad Osuyos <124412006+wllmstd@users.noreply.github.com>
2025-03-07 12:11:53 +0800

    Fix: Fixed Creating Requests

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -13,12 +13,16 @@ use Illuminate\Support\Facades\Log;
 
 class RequestController extends Controller
 {
-public function index()
-{
-    $user = Auth::user(); // Get the authenticated user
-    return view('user.usermanage', compact('requests'));
-}
-
+    public function index()
+    {
+        $userId = auth()->id(); // Get the logged-in user's ID
+    
+        $requests = UserRequest::where('Users_ID', $userId) // Get only requests from the logged-in user
+            ->get();
+    
+            return view('user.usermanage', compact('requests'));
+        }
+    
     
     public function destroy($id)
     {

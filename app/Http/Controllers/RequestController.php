6273f170b51fcae802c4ab1d21bf770af2daec96
kclementir <124416160+kclementir@users.noreply.github.com>
2025-03-07 14:56:34 +0800

    fix: error public function index

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -15,7 +15,7 @@ class RequestController extends Controller
 {
     public function index()
     {
-        $userId = auth()->id(); // Get the logged-in user's ID
+        $userId = auth::id(); // Get the logged-in user's ID
     
         $requests = UserRequest::where('Users_ID', $userId) // Get only requests from the logged-in user
             ->get();
